192520ee1f4f79848908f9264b089f1cc8be000d
Philippe ROUSSILLE <117383554+PhilR3iL@users.noreply.github.com>
2022-12-06 13:19:59 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+import exo11.PetitMain;
+
+class Main {
+  public static void main(String[] args) {
+    exo11.PetitMain.main();
+  }
+}
---
+++ exo11/PetitMain.java
@@ -0,0 +1,8 @@
+package exo11;
+
+public class PetitMain {
+  public static void main() {
+    Triplet<String> t = new Triplet<>("riri", "fifi", "loulou");
+    t.afficher();
+  }
+}
